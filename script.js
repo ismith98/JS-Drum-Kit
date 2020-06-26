be3b7c15f543bf70d3e90627f76a37f049649577
ibsby <16ismith98@gmail.com>
2020-06-26 04:52:50 -0400

    Transitions on keydown. Event listener for transition end on each key.

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 document.addEventListener("keydown", (e) => {
 	//console.log(e.keyCode);
+	
+	//Get the html element of the key that was pressed
 	var key = document.querySelector(`.key[data-key='${e.keyCode}']`);
+	//Get the html element of the audio for the key that was pressed
 	console.log(key);
-	key.classList.add(".playing");
+	key.classList.add("playing");
+	key.addEventListener("transitionend", e => {
+		if(e.propertyName != "transform" ) return;
+		
+		key.classList.remove("playing");
+		console.log(e);
+	})
+	
 })
